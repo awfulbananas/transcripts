c042227df391b706f4df09f92a0158a49cc45ede
Mark Verrey <mverrey@gmail.com>
2023-12-19 19:43:51 -0800

    Add site map
    Maybe there's a way to do this automatically, but it would at the very
    least require telling Next.js all the different possible parameters,
    similar to static page generation

---
+++ app/sitemap.ts
@@ -0,0 +1,53 @@
+import { MetadataRoute } from 'next';
+import { getAllCategories, getAllVideosForCategory, getDatesForCategory } from 'utilities/metadata-utils';
+import { getCategoryPath, getDatePath, getVideoPath } from 'utilities/path-utils';
+
+type ChangeFrequency = 'always' | 'hourly' | 'daily' | 'weekly' | 'monthly' | 'yearly' | 'never';
+
+type SiteMapEntry = {
+    url: string;
+    lastModified?: string | Date;
+    changeFrequency?: ChangeFrequency;
+    priority?: number;
+};
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const categories: string[] = await getAllCategories();
+
+  const videoPages: SiteMapEntry[] = [];
+
+  for (const category of categories) {
+    const videos = await getAllVideosForCategory(category);
+
+    videoPages.push(...videos.map(v => ({
+      url: getVideoPath(category, v.metadata.video_id),
+      lastModified: v.date,
+      changeFrequency: 'yearly' as ChangeFrequency,
+      priority: 1.0,
+    })));
+  }
+
+  const datePages: SiteMapEntry[] = [];
+
+  for (const category of categories) {
+    const dates = await getDatesForCategory(category);
+
+    datePages.push(...dates.map(d => ({
+      url: getDatePath(category, d),
+      lastModified: d,
+      changeFrequency: 'weekly' as ChangeFrequency,
+    })));
+  }
+
+  const categoryPages: SiteMapEntry[] = categories.map(c => ({
+    url: getCategoryPath(c),
+    changeFrequency: 'montly' as ChangeFrequency,
+    // TODO: Add last-modified, either a hard-coded value, or something determined from changelog
+  }));
+
+  return [
+    ...categoryPages,
+    ...datePages,
+    ...videoPages
+  ];
+}
