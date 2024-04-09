19bc731c87ca9af2b2be7a12fda52b91aae4c39e
awfulbananas <Joseph.Fromel@gmail.com>
2024-04-08 17:34:49 -0700

    fixed up yt audio and and metadata download script

--- tools/download_yt_audio.py
+++ tools/download_yt_audio.py
@@ -25,6 +25,9 @@
 # /mount_point/a/ab123.mp4
 # /mount_point/-/-1abd.mp4
 
+#for testing, I'm using command line args
+#py download_yt_audio.py -o "C:\Users\Joseph\Documents\test_out" -u "https://www.youtube.com/user/wanderbots/" -d
+
 from pytube import YouTube
 from pytube import Playlist
 from pytube import Channel
@@ -41,7 +44,7 @@ import traceback
 
 def get_outfile_base(outdir, video_id):
     """Returns the output file path for video_id without extension"""
-    return os.path.join(args.outdir, video_id[0], video_id)
+    return os.path.join(outdir, video_id[0], video_id)
 
 
 def is_json_file(path):
@@ -53,82 +56,105 @@ def is_json_file(path):
     except:
             return False
 
-parser = argparse.ArgumentParser(
-        prog='Youtube Audio Download',
-        description='Downloads audio from youtube videos in a channel or playlist')
-parser.add_argument('-o', '--outdir', dest='outdir', metavar='OUTDIR',
-                    help='write files in structure under OUTDIR', required=True)
-parser.add_argument('-u', '--url', dest='url', metavar="URL",
-                    help='URL of playlist or channel  to download from', required=True)
-parser.add_argument('-d', '--debug', dest='debug', help='Enable debug logging', action=argparse.BooleanOptionalAction)
-parser.add_argument('-s', '--skip-existing', dest='skip', help='Skip download of existing audio files. Only update metadata.', action=argparse.BooleanOptionalAction)
+def main():
+    parser = argparse.ArgumentParser(
+            prog='Youtube Audio Download',
+            description='Downloads audio from youtube videos in a channel or playlist')
+    parser.add_argument('-o', '--outdir', dest='outdir', metavar='OUTDIR',
+                        help='write files in structure under OUTDIR', required=True)
+    parser.add_argument('-u', '--url', dest='url', metavar="URL",
+                        help='URL of playlist or channel  to download from', required=True)
+    parser.add_argument('-d', '--debug', dest='debug', help='Enable debug logging', action=argparse.BooleanOptionalAction)
+    parser.add_argument('-s', '--skip-existing', dest='skip', help='Skip download of existing audio files. Only update metadata.', action=argparse.BooleanOptionalAction)
+    
+    args = parser.parse_args()
+    if not args.url or not args.outdir:
+        parser.print_help()
+        sys.exit(1)
+    
+    videos = get_videos(args.url)
+    
+    for v in range(3):
+        print(videos[v])
+        manage_video(videos[v], args.outdir)
+    
 
-args = parser.parse_args()
-if not args.url or not args.outdir:
-    parser.print_help()
-    sys.exit(1)
+def get_videos(url):
+    # Check URL structure against the smae regex pytube uses for checking if a url is a channel, otherwise, assume it's a playlist
+    patterns = [
+        r"(?:\/(c)\/([%\d\w_\-]+)(\/.*)?)",
+        r"(?:\/(channel)\/([%\w\d_\-]+)(\/.*)?)",
+        r"(?:\/(u)\/([%\d\w_\-]+)(\/.*)?)",
+        r"(?:\/(user)\/([%\w\d_\-]+)(\/.*)?)"
+    ]
+    isChannelURL = False;
+    for pattern in patterns:
+        regex = re.compile(pattern)
+        function_match = regex.search(url)
+        if function_match:
+            isChannelURL = True;
+            break
+    if isChannelURL:
+        videos = Channel(url).video_urls
+    else:
+        videos = Playlist(url).videos
+    
+    return videos
 
-if args.debug:
-    logging.getLogger().setLevel(logging.DEBUG)
-else:
-    logging.getLogger().setLevel(logging.INFO)
-
-# Check URL structure for /c/ to guess if it is a channel. Otherwise
-# assume it is a playlist.
-if re.match(r'.*/c|channels/.*', args.url):
-    videos = Channel(args.url).video_urls
-else:
-    videos = Playlist(args.url).videos
-print(videos)
-
-raise "foo"
-
-for v in videos:
+def manage_video(v, outdir):
     # Check if json file exists for video and is parseable.
-    video_id = v.video_id
-    outfile_base = get_outfile_base(args.outdir, video_id)
+    video_id = v.split('=')[1]
+    outfile_base = get_outfile_base(outdir, video_id)
     metadata_path = '%s.metadata.json' % outfile_base
-    logging.debug("Metadatafile: %s" % metadata_path)
+    
     if is_json_file(metadata_path):
         logging.info("Skipping %s" % video_id)
     else:
         logging.info("Downloading %s" % video_id)
-
+        
         try:
-            # Ensure the files are there.
-            outfile_name = '%s.mp4' % video_id
-            outfile_dir = os.path.dirname(outfile_base)
-            os.makedirs(outfile_dir, exist_ok=True)
-
-            # Do the download, always overwriting. The parseable metadata file is the
-            # completion flag.
-            audio_streams = v.streams.filter(only_audio=True).order_by('abr')
-            audio_streams.first().download(
-                    output_path=outfile_dir,
-                    filename=outfile_name,
-                    max_retries=5,
-                    skip_existing=args.skip)
-
-            # Download completed. Time to write metadata.
-            with open(metadata_path, "w") as f:
-                metadata = {
-                    'title': v.title,
-                    'video_id': v.video_id,
-                    'channel_id': v.channel_id,
-                    'description': v.description,
-                    'publish_date': v.publish_date.isoformat(),
-                }
-                print(metadata)
-                json.dump(metadata, f)
-            logging.info("Done Downloading %s" % video_id)
+            save_video(video_id, metadata_path, outfile_base)
+            
         except Exception as e:
             print("Download of %s Failed with exception %s" % (video_id, e))
             traceback.print_exc()
-    logging.info("Done Downloading %s" % video_id)
     pause_secs = randint(1,5)
-    logging.info("Pausing %d" % pause_secs)
     sleep(pause_secs)
 
+def save_video(video_id, metadata_path, outfile_base):
+    vid = YouTube.from_id(video_id)
+    
+    # Ensure the files are there.
+    outfile_name = '%s.mp4' % video_id
+    outfile_dir = os.path.dirname(outfile_base)
+    os.makedirs(outfile_dir, exist_ok=True)
+    
+    #don't download the audio, but I'm not deleting this yet in case I turn out to need it later
+    if(False):
+        # Do the download, always overwriting. The parseable metadata file is the
+        # completion flag.
+        audio_streams = vid.streams.filter(only_audio=True).order_by('abr')
+        audio_streams.first().download(
+                output_path=outfile_dir,
+                filename=outfile_name,
+                max_retries=5,
+                #this is meant to be based on a command line argument, but this is fine for now
+                skip_existing=True)
+    
+    # Download completed. Time to write metadata.
+    with open(metadata_path, "w") as f:
+        metadata = {
+            'title': vid.title,
+            'video_id': vid.video_id,
+            'channel_id': vid.channel_id,
+            'description': vid.description,
+            'publish_date': vid.publish_date.isoformat(),
+        }
+        print(metadata)
+        json.dump(metadata, f)
+    logging.info("Done Downloading %s" % video_id)
 
+if(__name__ == "__main__"):
+    main()
 
 
